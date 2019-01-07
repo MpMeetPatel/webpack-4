8d7893a9c6f97e420c8b12d16e630b253a3b41ac
MpMeetPatel <meetpatel.professional@gmail.com>
2019-01-07 22:49:55 +0530

    minor fixes

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
 import $ from "jquery";
 import add from "./temp";
 
+import "materialize-css/dist/css/materialize.min.css";
 import "../scss/main.scss";
 
 $(document).ready(() => {
-   console.log(add(5 + 5));
+   console.log(add(5, 5));
 });
